fcaea71b2eebce6adaf6ee71117a66353aa8c1c6
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2024-03-12 17:01:46 -0400

    organized config files
    Organized config files to divide responsibility

---
+++ src/main/java/com/crm/smsmanagementservice/config/CallbackProperties.java
@@ -0,0 +1,27 @@
+package com.crm.smsmanagementservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : memo-aldu
+ * @mailto : maldu064@uOttawa.ca
+ * @created : 3/1/2024, Friday
+ */
+@Configuration
+public class CallbackProperties {
+    @Value("${app.callback.url}")
+    private String url;
+    @Value("${app.callback.endpoints.smsStatus}")
+    private String smsStatusEndpoint;
+    @Value("${app.callback.endpoints.health}")
+    private String healthEndpoint;
+
+    public String getSmsStatusEndpoint() {
+        return url + smsStatusEndpoint;
+    }
+
+    public String getHealthEndpoint() {
+        return url + healthEndpoint;
+    }
+}
