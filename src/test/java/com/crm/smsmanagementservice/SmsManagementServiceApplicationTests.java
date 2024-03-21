e0ac45071e294e75e494cf0000eef2a01dc7325c
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2024-03-21 17:43:25 -0400

    Unit/IT test
    Reached 80%+ test coverage

--- src/test/java/com/crm/smsmanagementservice/SmsManagementServiceApplicationTests.java
+++ src/test/java/com/crm/smsmanagementservice/SmsManagementServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.crm.smsmanagementservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
 class SmsManagementServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
