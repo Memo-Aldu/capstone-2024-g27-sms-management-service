6850d0ca1c926dee69a12bb3f2fc31c313cd2948
Memo-Aldu <maldu064@uOttawa.ca>
2024-04-05 17:58:56 -0400

    Aded shell scripts to run the app

---
+++ start-prod.sh
@@ -0,0 +1,13 @@
+# Set the environment variable for the .env file
+env_file=./.env.prod
+
+set -a # Automatically export all variables
+source ./.env
+source $env_file
+set +a # Stop automatically exporting
+
+ENV_FILE=$env_file docker-compose --env-file $env_file up --build
+
+# Pause the script execution to keep the terminal open (similar to Windows' pause)
+echo "Press any key to continue..."
+read -k1 -s -r
